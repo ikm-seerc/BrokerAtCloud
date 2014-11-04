e4922d1765b961e7ad35252a9e4090fb8c58faa3
Chris Petsos <chrispetsos@msn.com>
2014-11-04 20:50:45 +0200

    Implemented the logic in BrokerPolicyValidator handler to validate a BP when it is PUT inside GReg at the specified path. Using a PolicyCompletenessCompliance object to do that. Added the BrokerAtCloud WSO2 Composite Application project which registers the BrokerPolicyValidationHandler inside GReg. BrokerPolicyValidationHandler project references PolicyCompletenessCompliance project for compile time. However, for runtime, its classes (along with the Jena classes) should be made available explicitely. See Git Repository Readme.md instructions on how to do this.

--- BrokerPolicyValidationHandler/src/main/java/org/seerc/handlers/BrokerPolicyValidator.java
+++ BrokerPolicyValidationHandler/src/main/java/org/seerc/handlers/BrokerPolicyValidator.java
@@ -1,13 +1,31 @@
 package org.seerc.handlers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import org.seerc.brokeratcloud.policycompletenesscompliance.BrokerPolicyException;
+import org.seerc.brokeratcloud.policycompletenesscompliance.PolicyCompletenessCompliance;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
-import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
 import org.wso2.carbon.registry.core.jdbc.handlers.Handler;
+import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
 
 public class BrokerPolicyValidator extends Handler {
 
 	public void put(RequestContext requestContext) throws RegistryException{
-		System.out.println("!!!!!!!!!!!!!!! put() called !!!!!!!!!!!!!!!!!!");
+		InputStream resourceIS = requestContext.getResource().getContentStream();
+		PolicyCompletenessCompliance pcc = new PolicyCompletenessCompliance();
+		try {
+			pcc.validateBrokerPolicy(resourceIS);
+		} catch (NoSuchMethodException | ClassNotFoundException
+				| InstantiationException | IllegalAccessException
+				| InvocationTargetException | IOException
+				| BrokerPolicyException e)
+		{
+			e.printStackTrace();
+			
+			throw new RegistryException(e.getMessage(), e);			
+		}
 		super.put(requestContext);
 	}
 
