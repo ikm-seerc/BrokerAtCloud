48cfba9159127ff68cc0e531f3795b11ca8d6228
Chris Petsos <chrispetsos@msn.com>
2014-11-25 19:51:06 +0200

    Extracted Fuseki datasetURL to external .properties file.

--- MessageBroker/src/org/seerc/brokeratcloud/messagebroker/FusekiClient.java
+++ MessageBroker/src/org/seerc/brokeratcloud/messagebroker/FusekiClient.java
@@ -3,6 +3,7 @@ package org.seerc.brokeratcloud.messagebroker;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 
@@ -10,11 +11,24 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.DatasetAccessor;
 import com.hp.hpl.jena.query.DatasetAccessorFactory;
-import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class FusekiClient {
 
+	String datasetURL;
+	
+	public FusekiClient(){
+		Properties fuseki_properties = new Properties();
+
+		try {
+			fuseki_properties.load(this.getClass().getResourceAsStream("/properties/fuseki.properties"));
+			datasetURL = fuseki_properties.getProperty("datasetURL");
+		} catch (IOException e) {
+			System.err.println("Could not load properties file from: /properties/fuseki.properties");
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		FusekiClient fc = new FusekiClient();
 		try {
@@ -26,7 +40,7 @@ public class FusekiClient {
 	
 	public void addInputStreamToFuseki(InputStream stream)
 	{
-		DatasetAccessor dataAccessor = DatasetAccessorFactory.createHTTP("http://localhost:3030/ds/data");
+		DatasetAccessor dataAccessor = DatasetAccessorFactory.createHTTP(datasetURL + "/data");
 
 		OntModel ontmodel = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
 		ontmodel.read(stream, null, "TTL");
