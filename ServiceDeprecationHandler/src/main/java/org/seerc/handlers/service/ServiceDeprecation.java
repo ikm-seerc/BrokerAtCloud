10534d6a63cf9403dbe491591324706d34d21b5a
chrispetsos <chrispetsos@msn.com>
2015-02-24 17:20:58 +0200

    Send the service deprecation event when a service gets deprecated.

--- ServiceDeprecationHandler/src/main/java/org/seerc/handlers/service/ServiceDeprecation.java
+++ ServiceDeprecationHandler/src/main/java/org/seerc/handlers/service/ServiceDeprecation.java
@@ -1,6 +1,12 @@
 package org.seerc.handlers.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.seerc.brokeratcloud.messagebroker.FusekiClient;
+import org.seerc.brokeratcloud.messagebroker.ServiceLifecyclePublisher;
+import org.seerc.brokeratcloud.messagebroker.WSO2GREGEvaluator;
+import org.seerc.brokeratcloud.policycompletenesscompliance.CompletenessException;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
 import org.wso2.carbon.registry.core.jdbc.handlers.Handler;
@@ -9,15 +15,31 @@ public class ServiceDeprecation extends Handler {
 
 	FusekiClient fc;
 	
+	ServiceLifecyclePublisher slp;
+	
+	WSO2GREGEvaluator gregEvaluator;
+
 	public ServiceDeprecation()
 	{
 		fc = new FusekiClient();
+		
+		slp = new ServiceLifecyclePublisher();
+		
+		gregEvaluator = new WSO2GREGEvaluator();
 	}
 	
 	public void delete(RequestContext requestContext) throws RegistryException{
 		// Delete it first from fuseki
 		System.out.println("Deleting " + requestContext.getResourcePath() + " from Fuseki.");
-		fc.deleteInputStreamFromFuseki(requestContext.getRegistry().get(requestContext.getResourcePath().getCompletePath()).getContentStream());
+		InputStream contentStream = requestContext.getRegistry().get(requestContext.getResourcePath().getCompletePath()).getContentStream();
+		fc.deleteInputStreamFromFuseki(contentStream);
+		
+		// send the service deprecation event
+		try {
+			slp.serviceDeprecated(gregEvaluator.getPcc().getSDServiceInstanceURI(contentStream));
+		} catch (IOException | CompletenessException e) {
+			e.printStackTrace();
+		}
 		
 		// Then delete resource from GReg
 		super.delete(requestContext);
