829b7df811669e7d627746701aa5e8c54346fc36
chrispetsos <chrispetsos@msn.com>
2015-02-24 16:10:02 +0200

    Created ServiceLifecyclePublisher which is resposnible for publishing to the appropriate topics messages about services that have been created, updated or deprecated.

---
+++ MessageBroker/src/org/seerc/brokeratcloud/messagebroker/ServiceLifecyclePublisher.java
@@ -0,0 +1,50 @@
+package org.seerc.brokeratcloud.messagebroker;
+
+import com.google.gson.JsonObject;
+
+public class ServiceLifecyclePublisher {
+
+	private static String serviceOnboardedTopic = "serviceOnboardedTopic"; 
+	private static String serviceUpdatedTopic = "serviceUpdatedTopic"; 
+	private static String serviceDeprecatedTopic = "serviceDeprecatedTopic"; 
+
+	MessageBrokerStringPublisher onboardedPublisher;
+	MessageBrokerStringPublisher updatedPublisher;
+	MessageBrokerStringPublisher deprecatedPublisher;
+	
+	public ServiceLifecyclePublisher()
+	{
+		this.onboardedPublisher = new MessageBrokerStringPublisher("onboardedPublisher", serviceOnboardedTopic);
+		this.updatedPublisher = new MessageBrokerStringPublisher("updatedPublisher", serviceUpdatedTopic);
+		this.deprecatedPublisher = new MessageBrokerStringPublisher("deprecatedPublisher", serviceDeprecatedTopic);
+	}
+	
+	public static void main(String[] args) {
+		ServiceLifecyclePublisher slp = new ServiceLifecyclePublisher();
+		
+		slp.serviceOnboarded("http://the.onboarded.service/completeURI#theFragment");
+		slp.serviceUpdated("http://the.updated.service/completeURI#theFragment");
+		slp.serviceDeprecated("http://the.deprecated.service/completeURI#theFragment");
+	}
+	
+	private String constructJSONServiceEvent(String serviceID)
+	{
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("serviceID", serviceID);
+		
+		return jsonObject.toString();
+	}
+
+	public void serviceOnboarded(String serviceID) {
+		this.onboardedPublisher.publishStringToTopic(constructJSONServiceEvent(serviceID));
+	}
+
+	public void serviceUpdated(String serviceID) {
+		this.updatedPublisher.publishStringToTopic(constructJSONServiceEvent(serviceID));
+	}
+
+	public void serviceDeprecated(String serviceID) {
+		this.deprecatedPublisher.publishStringToTopic(constructJSONServiceEvent(serviceID));
+	}
+
+}
