8fcb16e1183a56c266f996e49bbb08d17b811e93
chrispetsos <chrispetsos@msn.com>
2015-03-04 21:40:01 +0200

    Added REST API for uploading BP.

--- org.seerc.brokeratcloud.webservice/src/org/seerc/brokeratcloud/webservice/BrokerPolicyValidator.java
+++ org.seerc.brokeratcloud.webservice/src/org/seerc/brokeratcloud/webservice/BrokerPolicyValidator.java
@@ -1,19 +1,34 @@
 package org.seerc.brokeratcloud.webservice;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
+import org.seerc.brokeratcloud.messagebroker.WSO2GREGClient;
+import org.seerc.brokeratcloud.policycompletenesscompliance.BrokerPolicyException;
 import org.seerc.brokeratcloud.policycompletenesscompliance.PolicyCompletenessCompliance;
+import org.wso2.carbon.registry.core.Resource;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
 
-@Path("/brokerPolicy/validate")
+@Path("/brokerPolicy")
 public class BrokerPolicyValidator {
 
+	WSO2GREGClient greg;
+	
+	public BrokerPolicyValidator()
+	{
+		this.greg = new WSO2GREGClient();
+	}
+	
 	@POST
-	@Path("/")
-	public String publishToTopic(String bpContents)
+	@Path("/validate")
+	public String validateBP(String bpContents)
 	{
 		try {
 			PolicyCompletenessCompliance pcc = new PolicyCompletenessCompliance();
@@ -28,4 +43,31 @@ public class BrokerPolicyValidator {
 		
 		return "OK";
 	}
+	
+	@PUT
+	@Path("/upload")
+	public String uploadBP(String bpContents) throws RegistryException, URISyntaxException, IOException, BrokerPolicyException
+	{
+		PolicyCompletenessCompliance pcc = new PolicyCompletenessCompliance();
+		String validationResult = this.validateBP(bpContents);
+		if(validationResult.equals("OK"))
+		{
+			Resource resourceForName = this.greg.getRemote_registry().newResource();
+			resourceForName.setMediaType("text/plain");
+			resourceForName.setContent(bpContents);
+			String bpName = new URI(pcc.getBPInstanceUri(resourceForName.getContentStream())).getFragment() + ".ttl";
+			//resource.getContentStream().reset();
+			Resource resource = this.greg.getRemote_registry().newResource();
+			resource.setMediaType("text/plain");
+			resource.setContent(bpContents);
+			this.greg.putWithRetryHack(WSO2GREGClient.getBrokerPoliciesFolder() + bpName, resource);
+
+			return "OK";
+		}
+		else 
+		{
+			return validationResult;
+		}
+	}
+	
 }
