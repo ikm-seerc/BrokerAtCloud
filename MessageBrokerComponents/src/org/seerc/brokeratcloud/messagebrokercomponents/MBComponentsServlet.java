46192df3e265de0d6936aead5a430c77208bcce5
chrispetsos <chrispetsos@msn.com>
2015-04-28 17:57:31 +0300

    Added a helper script prior starting MB components, in order to create needed paths in GReg, if they don't exist.

--- MessageBrokerComponents/src/org/seerc/brokeratcloud/messagebrokercomponents/MBComponentsServlet.java
+++ MessageBrokerComponents/src/org/seerc/brokeratcloud/messagebrokercomponents/MBComponentsServlet.java
@@ -6,11 +6,19 @@ import javax.servlet.http.HttpServlet;
 import org.seerc.brokeratcloud.messagebroker.EvaluationComponentSDSubscriber;
 import org.seerc.brokeratcloud.messagebroker.OutOfRangeSLAViolationChecker;
 import org.seerc.brokeratcloud.messagebroker.RegistryRepositoryTopicSubscriber;
+import org.seerc.brokeratcloud.messagebroker.WSO2GREGClient;
+import org.wso2.carbon.registry.core.Collection;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
 
 public class MBComponentsServlet extends HttpServlet {
 
 	public void init() throws ServletException
     {
+		try {
+			this.createNeededPathsInGReg();
+		} catch (RegistryException e) {
+			e.printStackTrace();
+		}
 		RegistryRepositoryTopicSubscriber.main(null);
 		EvaluationComponentSDSubscriber.main(null);
 		OutOfRangeSLAViolationChecker.main(null);
@@ -19,4 +27,31 @@ public class MBComponentsServlet extends HttpServlet {
         System.out.println("---------- MBComponentsServlet Servlet Initialized successfully ----------");
         System.out.println("----------");
     }
+
+	private void createNeededPathsInGReg() throws RegistryException {
+		String brokerAtCloudFolderPath = "/brokerAtCloud";
+		String brokerPoliciesFolderPath = "/brokerAtCloud/brokerPolicies";
+		String serviceDescriptionsFolderPath = "/brokerAtCloud/serviceDescriptions";
+		
+		WSO2GREGClient greg = new WSO2GREGClient();
+		
+		if(!greg.getRemote_registry().resourceExists(brokerAtCloudFolderPath))
+		{
+			Collection brokerAtCloudFolder = greg.getRemote_registry().newCollection();
+			greg.putWithRetryHack(brokerAtCloudFolderPath, brokerAtCloudFolder);
+		}
+		
+		if(!greg.getRemote_registry().resourceExists(brokerPoliciesFolderPath))
+		{
+			Collection brokerPoliciesFolder = greg.getRemote_registry().newCollection();
+			greg.putWithRetryHack(brokerPoliciesFolderPath, brokerPoliciesFolder);
+		}
+		
+		if(!greg.getRemote_registry().resourceExists(serviceDescriptionsFolderPath))
+		{
+			Collection serviceDescriptionsFolder = greg.getRemote_registry().newCollection();
+			greg.putWithRetryHack(serviceDescriptionsFolderPath, serviceDescriptionsFolder);
+		}
+		
+	}
 }
