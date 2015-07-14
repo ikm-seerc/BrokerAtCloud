96f058ba8b4e394ae9ed1b733063872ed3149609
chrispetsos <chrispetsos@msn.com>
2015-07-14 12:38:47 +0300

    Added stubs in VttsPubSubBridge for verification and generation. The needed REST services are not ready yet...

--- MessageBroker/src/org/seerc/brokeratcloud/messagebroker/VttsPubSubBridge.java
+++ MessageBroker/src/org/seerc/brokeratcloud/messagebroker/VttsPubSubBridge.java
@@ -15,8 +15,20 @@ public class VttsPubSubBridge {
 	MessageBrokerStringPublisher vttsValidationReportPublisher;
 	MessageBrokerSubscriber vttsValidationSubscriber;
 	
+	MessageBrokerStringPublisher vttsVerificationReportPublisher;
+	MessageBrokerSubscriber vttsVerificationSubscriber;
+	
+	MessageBrokerStringPublisher vttsGenerationReportPublisher;
+	MessageBrokerSubscriber vttsGenerationSubscriber;
+	
 	public VttsPubSubBridge()
 	{
+		createVTTSValidationSubscriber();
+		createVTTSVerificationSubscriber();
+		createVTTSGenerationSubscriber();
+	}
+
+	private void createVTTSValidationSubscriber() {
 		vttsValidationReportPublisher = new MessageBrokerStringPublisher("vttsValidationReportPublisher", "vttsValidationReport");
 		vttsValidationSubscriber = new MessageBrokerSubscriber("vttsValidationSubscriber", "vttsValidation", new MessageListener() {
 			
@@ -51,6 +63,80 @@ public class VttsPubSubBridge {
 			}
 		}, false);
 		vttsValidationSubscriber.subscribeToTopic();
+	}
+	
+	private void createVTTSVerificationSubscriber() {
+		vttsVerificationReportPublisher = new MessageBrokerStringPublisher("vttsVerificationReportPublisher", "vttsVerificationReport");
+		vttsVerificationSubscriber = new MessageBrokerSubscriber("vttsVerificationSubscriber", "vttsVerification", new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				// message received from PubSub
+				try {
+					System.out.println("vttsVerificationSubscriber received the message with ID==> "
+							+ message.getJMSMessageID());
+					BytesMessage bm = (BytesMessage) message;
+					
+					// get its bytes
+					ByteArrayOutputStream bOutput = new ByteArrayOutputStream();
+					int i;
+					while((i=bm.readInt())!=-1){
+						bOutput.write(i);
+					}
+					
+					// The document as a String
+					String document = bOutput.toString();
+					
+					// send for validation
+					String response = HttpPostClient.postMessageToEndpoint(serverAddress + "/vtts/rest/VTTSEngine/verification", document);
+					
+					// publish report to PubSub
+					vttsVerificationReportPublisher.publishStringToTopic(response);
+
+				} catch (JMSException | IOException e) {
+					e.printStackTrace();
+					vttsVerificationReportPublisher.publishStringToTopic(e.getMessage());
+				}
+			}
+		}, false);
+		vttsVerificationSubscriber.subscribeToTopic();
+	}
+	
+	private void createVTTSGenerationSubscriber() {
+		vttsGenerationReportPublisher = new MessageBrokerStringPublisher("vttsGenerationReportPublisher", "vttsGenerationReport");
+		vttsGenerationSubscriber = new MessageBrokerSubscriber("vttsGenerationSubscriber", "vttsGeneration", new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				// message received from PubSub
+				try {
+					System.out.println("vttsGenerationSubscriber received the message with ID==> "
+							+ message.getJMSMessageID());
+					BytesMessage bm = (BytesMessage) message;
+					
+					// get its bytes
+					ByteArrayOutputStream bOutput = new ByteArrayOutputStream();
+					int i;
+					while((i=bm.readInt())!=-1){
+						bOutput.write(i);
+					}
+					
+					// The document as a String
+					String document = bOutput.toString();
+					
+					// send for validation
+					String response = HttpPostClient.postMessageToEndpoint(serverAddress + "/vtts/rest/VTTSEngine/generation", document);
+					
+					// publish report to PubSub
+					vttsGenerationReportPublisher.publishStringToTopic(response);
+
+				} catch (JMSException | IOException e) {
+					e.printStackTrace();
+					vttsGenerationReportPublisher.publishStringToTopic(e.getMessage());
+				}
+			}
+		}, false);
+		vttsGenerationSubscriber.subscribeToTopic();
 	}
 	
 	public static void main(String[] args) {
