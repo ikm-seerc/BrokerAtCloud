7bff549a10e4180f0898a86d2ac4cb4e1d630672
chrispetsos <chrispetsos@msn.com>
2015-10-14 14:20:47 +0300

    Added some functions for performance testing.

--- MessageBroker/src/org/seerc/brokeratcloud/messagebroker/HttpPostClient.java
+++ MessageBroker/src/org/seerc/brokeratcloud/messagebroker/HttpPostClient.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+
+import org.wso2.andes.util.FileUtils;
 
 public class HttpPostClient {
 	public static String postMessageToEndpoint(String endpoint, String msg) throws IOException
@@ -38,5 +41,26 @@ public class HttpPostClient {
 		httpConnection.disconnect();
 		
 		return responseContent;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Date().getTime());
+		testMultipleRequests(5);
+	}
+
+	private static void testMultipleRequests(int numberOfRequests) {
+		for(int i=0;i<numberOfRequests;i++)
+		{
+			testSingleRequest();
+		}
+	}
+	
+	private static void testSingleRequest() {
+		try {
+			HttpPostClient.postMessageToEndpoint("http://213.249.38.66:3335/org.seerc.brokeratcloud.webservice/rest/topics/evaluation/SD/seerc", 
+					FileUtils.readFileAsString("../PolicyCompletenessCompliance/Ontologies/Current/CAS-AddressApp1.ttl"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
